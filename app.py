2977c3181bcb565e916ca2897d5efc4bf7d1660b
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-19 13:09:45 +0530

    app.py added

--- app.py
+++ app.py
@@ -4,53 +4,23 @@ import numpy as np
 # from sklearn.externals import joblib
 import joblib
 
-
 # Load the trained model
 model = joblib.load('model.sav')
 
-# # Function to preprocess input data
-# def preprocess_input(data):
-#     # Convert binary categorical variables to numerical
-#     data['gender'] = data['gender'].map({'Male': 1, 'Female': 0})
-#     data['Partner'] = data['Partner'].map({'Yes': 1, 'No': 0})
-#     data['Dependents'] = data['Dependents'].map({'Yes': 1, 'No': 0})
-#     data['PhoneService'] = data['PhoneService'].map({'Yes': 1, 'No': 0})
-#     data['PaperlessBilling'] = data['PaperlessBilling'].map({'Yes': 1, 'No': 0})
-    
-#     # Convert categorical variables to dummy variables
-#     data = pd.get_dummies(data, columns=['MultipleLines', 'InternetService', 'Contract', 
-#                                          'PaymentMethod', 'OnlineSecurity', 'OnlineBackup', 
-#                                          'DeviceProtection', 'TechSupport', 'StreamingTV', 
-#                                          'StreamingMovies'])
-    
-#     # Fill missing values
-#     data['TotalCharges'] = data['TotalCharges'].replace(' ', np.nan).astype(float)
-#     data['TotalCharges'].fillna(data['TotalCharges'].mean(), inplace=True)
-    
-#     return data
 # Function to preprocess input data
 def preprocess_input(data):
-    # List of columns to one-hot encode
-    columns_to_encode = ['InternetService', 'Contract', 'PaymentMethod']
-    
-    # Convert binary categorical variables to numerical
-    data['gender'] = data['gender'].map({'Male': 1, 'Female': 0})
-    data['Partner'] = data['Partner'].map({'Yes': 1, 'No': 0})
-    data['Dependents'] = data['Dependents'].map({'Yes': 1, 'No': 0})
-    data['PhoneService'] = data['PhoneService'].map({'Yes': 1, 'No': 0})
-    data['PaperlessBilling'] = data['PaperlessBilling'].map({'Yes': 1, 'No': 0})
-    
     # Convert categorical variables to dummy variables
-    for column in columns_to_encode:
-        if column in data.columns:
-            data = pd.get_dummies(data, columns=[column])
+    categorical_columns = ['gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 
+                           'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 
+                           'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 
+                           'PaperlessBilling', 'PaymentMethod']
+    data = pd.get_dummies(data, columns=categorical_columns)
     
     # Fill missing values
     data['TotalCharges'] = data['TotalCharges'].replace(' ', np.nan).astype(float)
     data['TotalCharges'].fillna(data['TotalCharges'].mean(), inplace=True)
     
     return data
-
 
 # Define the layout of the app
 def main():
@@ -60,34 +30,39 @@ def main():
     st.write('Enter the customer details below to predict churn.')
 
     # Add input fields for user input
-    gender = st.selectbox('Gender', ['Male', 'Female'])
     senior_citizen = st.selectbox('Senior Citizen', ['No', 'Yes'])
-    partner = st.selectbox('Partner', ['Yes', 'No'])
-    dependents = st.selectbox('Dependents', ['Yes', 'No'])
-    tenure = st.slider('Tenure (months)', min_value=0, max_value=72, value=36)
-    phone_service = st.selectbox('Phone Service', ['Yes', 'No'])
-    internet_service = st.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'])
-    online_security = st.selectbox('Online Security', ['No', 'Yes', 'No internet service'])
-    online_backup = st.selectbox('Online Backup', ['No', 'Yes', 'No internet service'])
-    device_protection = st.selectbox('Device Protection', ['No', 'Yes', 'No internet service'])
-    tech_support = st.selectbox('Tech Support', ['No', 'Yes', 'No internet service'])
-    streaming_tv = st.selectbox('Streaming TV', ['No', 'Yes', 'No internet service'])
-    streaming_movies = st.selectbox('Streaming Movies', ['No', 'Yes', 'No internet service'])
-    contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
-    paperless_billing = st.selectbox('Paperless Billing', ['Yes', 'No'])
-    payment_method = st.selectbox('Payment Method', ['Electronic check', 'Mailed check', 
-                                                     'Bank transfer (automatic)', 'Credit card (automatic)'])
     monthly_charges = st.number_input('Monthly Charges ($)', min_value=0.0, max_value=200.0, value=50.0)
     total_charges = st.number_input('Total Charges ($)', min_value=0.0, max_value=10000.0, value=1000.0)
+    gender = st.selectbox('Gender', ['Female', 'Male'])
+    partner = st.selectbox('Partner', ['No', 'Yes'])
+    dependents = st.selectbox('Dependents', ['No', 'Yes'])
+    phone_service = st.selectbox('Phone Service', ['No', 'Yes'])
+    multiple_lines = st.selectbox('Multiple Lines', ['No', 'No phone service', 'Yes'])
+    internet_service = st.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'])
+    online_security = st.selectbox('Online Security', ['No', 'No internet service', 'Yes'])
+    online_backup = st.selectbox('Online Backup', ['No', 'No internet service', 'Yes'])
+    device_protection = st.selectbox('Device Protection', ['No', 'No internet service', 'Yes'])
+    tech_support = st.selectbox('Tech Support', ['No', 'No internet service', 'Yes'])
+    streaming_tv = st.selectbox('Streaming TV', ['No', 'No internet service', 'Yes'])
+    streaming_movies = st.selectbox('Streaming Movies', ['No', 'No internet service', 'Yes'])
+    contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
+    paperless_billing = st.selectbox('Paperless Billing', ['No', 'Yes'])
+    payment_method = st.selectbox('Payment Method', ['Bank transfer (automatic)', 
+                                                     'Credit card (automatic)', 
+                                                     'Electronic check', 
+                                                     'Mailed check'])
+    tenure = st.slider('Tenure (months)', min_value=1, max_value=72, value=36)
 
     # Create a dictionary with user inputs
     input_data = {
-        'gender': gender,
         'SeniorCitizen': senior_citizen,
+        'MonthlyCharges': monthly_charges,
+        'TotalCharges': total_charges,
+        'gender': gender,
         'Partner': partner,
         'Dependents': dependents,
-        'tenure': tenure,
         'PhoneService': phone_service,
+        'MultipleLines': multiple_lines,
         'InternetService': internet_service,
         'OnlineSecurity': online_security,
         'OnlineBackup': online_backup,
@@ -98,8 +73,7 @@ def main():
         'Contract': contract,
         'PaperlessBilling': paperless_billing,
         'PaymentMethod': payment_method,
-        'MonthlyCharges': monthly_charges,
-        'TotalCharges': total_charges
+        'tenure': tenure
     }
 
     # Convert input data into a DataFrame
